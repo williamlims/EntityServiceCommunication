2fbb922c8e5cc4ebe642b3fa926d08a86d3e8b50
William Lima <williamlims@hotmail.com>
2024-03-16 19:15:32 -0300

    feat: adding a new service for counting functionality

--- app/src/main/java/br/edu/ifsp/scl/sdm/entityservicecommunication/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/entityservicecommunication/MainActivity.kt
@@ -1,6 +1,8 @@
 package br.edu.ifsp.scl.sdm.entityservicecommunication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,18 +13,32 @@ class MainActivity : AppCompatActivity() {
     private val amb: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+    private lateinit var incrementServiceIntent: Intent
+    private var counter = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
+        incrementServiceIntent = Intent(this, IncrementService::class.java)
         with (amb) {
             mainTb.apply {
                 getString(R.string.app_name).also { setTitle(it) }
                 setSupportActionBar(this)
             }
             incrementBt.setOnClickListener {
-
+                startService(incrementServiceIntent.apply {
+                    putExtra("VALUE", counter)
+                })
             }
         }
+        InterEntityCommunication.valueLiveData.observe(this){ value ->
+            counter = value
+            Toast.makeText(this, "You clicked $counter times.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopService(incrementServiceIntent)
     }
 }
