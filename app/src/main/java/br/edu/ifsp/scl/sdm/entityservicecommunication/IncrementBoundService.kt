3fce57d32229a5ec823a6fab01c4d553095d82f4
William Lima <williamlims@hotmail.com>
2024-03-16 22:02:29 -0300

    feat: implements aidl for communication

--- app/src/main/java/br/edu/ifsp/scl/sdm/entityservicecommunication/IncrementBoundService.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/entityservicecommunication/IncrementBoundService.kt
@@ -1,5 +1,6 @@
 package br.edu.ifsp.scl.sdm.entityservicecommunication
 
+
 import android.app.Service
 import android.content.Intent
 import android.os.Handler
@@ -12,33 +13,14 @@ import android.util.Log
 
 class IncrementBoundService : Service() {
 
-    private inner class IncrementBoundServiceHandler(looper: Looper): Handler(looper){
-        override fun handleMessage(msg: Message) {
-            super.handleMessage(msg)
-
-            msg.replyTo?.also {
-                clientMessenger = it
-            }
-
-            msg.data.getInt("VALUE").also {
-                if (it != -1){
-                    Log.v(this.javaClass.simpleName, "Returning incremented value.")
-                    clientMessenger.send(Message.obtain().apply {
-                        data.putInt("VALUE", it + 1)
-                    })
-                }
-            }
-        }
+    private val ibsBinder: IncrementBoundServiceInterface.Stub = object: IncrementBoundServiceInterface.Stub() {
+        override fun increment(value: Int): Int = value + 1
     }
 
-    private lateinit var ibsMessenger: Messenger
-    private lateinit var ibsHandler: IncrementBoundServiceHandler
-    private lateinit var clientMessenger: Messenger
 
     override fun onBind(intent: Intent): IBinder {
         Log.v(this.javaClass.simpleName, "Entity bound to the service.")
-        ibsMessenger = Messenger(ibsHandler)
-        return ibsMessenger.binder
+        return ibsBinder
     }
 
     override fun onUnbind(intent: Intent): Boolean {
@@ -46,11 +28,4 @@ class IncrementBoundService : Service() {
         return super.onUnbind(intent)
     }
 
-    override fun onCreate() {
-        super.onCreate()
-        HandlerThread(this.javaClass.simpleName).apply {
-            start()
-            ibsHandler = IncrementBoundServiceHandler(looper)
-        }
-    }
 }
